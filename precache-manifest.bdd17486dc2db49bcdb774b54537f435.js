21432d480801bb374ed0c5fade46f6bd718dbf54
Gilang R <sinyo101520@gmail.com>
2020-07-25 01:40:10 +0700

    Updates

---
+++ precache-manifest.bdd17486dc2db49bcdb774b54537f435.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f86a148c68a99b797a87e9aa56946dd4",
+    "url": "/kokoh/index.html"
+  },
+  {
+    "revision": "480a88fbadc5421aebce",
+    "url": "/kokoh/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "a07e411f7a54db1055ba",
+    "url": "/kokoh/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "480a88fbadc5421aebce",
+    "url": "/kokoh/static/js/2.2b3586c2.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/kokoh/static/js/2.2b3586c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a07e411f7a54db1055ba",
+    "url": "/kokoh/static/js/main.1bc9c571.chunk.js"
+  },
+  {
+    "revision": "f79eec9c7b96429c463a",
+    "url": "/kokoh/static/js/runtime-main.af8c7e7e.js"
+  }
+]);
